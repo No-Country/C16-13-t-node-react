26ed653777d69db4afab9d9d61ffdf4dd2c46ffa
Roman Arenas <roman.ea3232@gmail.com>
2024-02-13 19:25:09 -0300

    model-controllers-news

--- BACKEND/dist/routes/notices.routes.js
+++ BACKEND/dist/routes/notices.routes.js
@@ -1,1 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const notices_controllers_1 = require("../controllers/notices.controllers");
+const noticesRouter = (0, express_1.Router)();
+noticesRouter.get('/news', notices_controllers_1.listarNoticias);
+exports.default = noticesRouter;
