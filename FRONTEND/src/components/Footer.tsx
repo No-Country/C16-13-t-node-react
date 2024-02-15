aeffc92e241fe2c005fc4efe2f2794c6cc37667d
estebancastano <estebin5372@gmail.com>
2024-02-15 16:33:45 -0500

    Agregado informacion

--- FRONTEND/src/components/Footer.tsx
+++ FRONTEND/src/components/Footer.tsx
@@ -1,6 +1,10 @@
 
 export const Footer = () => {
   return (
-    <div className="bg-gray-300">Footer</div>
+    <div className="bg-gray-300 flex justify-around">
+        <a href="">Links de interés</a>
+        <a href="">Redes sociales</a>
+        <a href="">Información legal</a>
+    </div>
   )
 }
