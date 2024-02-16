eaa4d0ecbf462b489cabb80561009211509d3751
fandres62 <fandres6262@gmail.com>
2024-02-16 15:26:37 -0500

    hh

--- FRONTEND/src/components/Footer.tsx
+++ FRONTEND/src/components/Footer.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 export const Footer = () => {
   return (
     <div className="bg-gray-300 flex justify-around">
+        <Link to="">Links</Link>
         <Link to="">Links de interés</Link>
         <Link to="">Redes sociales</Link>
         <Link to="">Información legal</Link>
