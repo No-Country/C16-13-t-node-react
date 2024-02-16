7c3807f45105662dc5a90c200f38b6a23b938f77
estebancastano <estebin5372@gmail.com>
2024-02-16 09:26:40 -0500

    Add icons search and menuham

--- FRONTEND/src/components/Navbar.tsx
+++ FRONTEND/src/components/Navbar.tsx
@@ -6,10 +6,16 @@ export const Navbar = () => {
     <nav className="flex flex-row justify-between p-2 bg-gray-300 h-20 items-center">
 
         <div>LOGO</div>
-        <div className="flex flex-row justify-center gap-2">
+        <div className="flex flex-row justify-center gap-4 m-6">
 
-            <div>LUPA</div>
-            <div>MENUHAMB</div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg>
+
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+
         </div>
     </nav>
   )
