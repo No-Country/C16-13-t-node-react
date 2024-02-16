5d733b52f1e5879a7f26207c4b6a2b1052324561
Roman Arenas <roman.ea3232@gmail.com>
2024-02-16 17:04:47 -0300

    front-fix

--- FRONTEND/src/components/Navbar.tsx
+++ FRONTEND/src/components/Navbar.tsx
@@ -1,41 +1,37 @@
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
-import { Menu } from '.';
-
+import { faBars, faSearch, faTimes, faMagnifyingGlass, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import {Menu, Busqueda } from './';
 
 export const Navbar = () => {
-
-  const [menuHamb, setMenuHamb] = useState(false)
+  const [menuHamb, setMenuHamb] = useState(false);
+  const [busqueda, setBusqueda] = useState(false);
 
   const mostrarMenu = () => {
-    setMenuHamb(!menuHamb)
+    setMenuHamb(!menuHamb);
   }
 
-  console.log(menuHamb)
-  return (
+  const iniciarBusqueda = () => {
+    setBusqueda(!busqueda);
+  }
 
+  return (
     <div className='flex flex-col'>
       <nav className="flex flex-row justify-between p-2 bg-gray-300 h-20 items-center">
-
         <div>LOGO</div>
-
-        <div className="flex flex-row justify-center gap-4 m-6">
-
-          <div>
-            <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+        <div className="flex flex-row justify-center gap-6 m-6">
+          <div onClick={iniciarBusqueda}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
           </div>
-
-          <div onClick={() => mostrarMenu()}>
-            {
-              menuHamb ? <FontAwesomeIcon icon={faXmark} /> : <FontAwesomeIcon icon={faBars} />
-            }
-
+          <div onClick={mostrarMenu}>
+            {menuHamb ? <FontAwesomeIcon icon={faTimesCircle} /> : <FontAwesomeIcon icon={faBars} />}
           </div>
         </div>
       </nav>
-      <Menu />
+      <div>
+        {busqueda && <Busqueda />}
+        {menuHamb && <Menu />}
+      </div>
     </div>
-
-  )
+  );
 }
