4d05e00e13f89f58f365865e7fb56a3459826969
estebancastano <estebin5372@gmail.com>
2024-02-24 20:52:10 -0500

    create login

--- FRONTEND/src/auth/Login.tsx
+++ FRONTEND/src/auth/Login.tsx
@@ -23,7 +23,7 @@ export const Login = () => {
         <label><input type="checkbox" /> Recuerdame</label>
         <a href="#">Recuperar contraseña</a>
       </div>
-      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Iniciar sesión</button>
+      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Iniciar Sesión</button>
       <li className="list-none">¿No tienes cuenta?<Link to="/register"> Regístrate</Link></li>
       <div className="inline-flex items-center justify-center w-full">
         <hr className="w-64 h-px my-8 bg-gray-900 border-0 dark:bg-gray-900" />
