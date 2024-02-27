b3facd0cd6c89d01e60099d48534895c9f2e3c0d
JosueXA <josue.xicotencatl.a@gmail.com>
2024-02-27 07:48:47 -0600

    add screen login

--- BACKEND/src/types/express/index.d.ts
+++ BACKEND/src/types/express/index.d.ts
@@ -8,3 +8,6 @@ declare global {
         }
     }
 }
+
+// Por este enlace se puede ver la solución al problema de typescript con express para la req.user en el middleware de validate-jwt
+// https://stackoverflow.com/questions/65848442/property-user-does-not-exist-on-type-requestparamsdictionary-any-any-pars
