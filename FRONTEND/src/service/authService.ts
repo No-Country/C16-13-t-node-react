9b199d4d62a458604f451fad426f087799a0795b
Roman Arenas <roman.ea3232@gmail.com>
2024-02-29 15:45:17 -0300

    autenticacion

--- FRONTEND/src/service/authService.ts
+++ FRONTEND/src/service/authService.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:8080/login';
+const baseURL = 'http://localhost:8080';
 
 const authService = {
     authenticateUser: async (logearUsuario: any) => {
-        const response = await axios.post(`${baseURL}`, logearUsuario);
-        return response.data.noticias;
+        const response = await axios.post(`${baseURL}/login`, logearUsuario);
+        return response.data;
     },
 };
 
