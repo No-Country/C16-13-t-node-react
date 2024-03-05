863ea3b5bd5a55f5dace248bede7dee7e66b8646
Roman Arenas <roman.ea3232@gmail.com>
2024-03-05 19:52:44 -0300

    global-context

--- FRONTEND/src/routes/AppRouter.tsx
+++ FRONTEND/src/routes/AppRouter.tsx
@@ -6,35 +6,53 @@ import { Login, Register } from '../auth';
 import { ResetPassword } from '../auth/ResetPassword';
 import { MockupLogin } from '../auth/Mockup';
 import { Footer, Navbar } from '../components';
+import { useContext } from 'react';
+import { UserContext } from '../context/UserProvider';
 
 export const AppRouter = () => {
 
+    const { user } = useContext(UserContext);
+    console.log(user)
+
+    const { rol } = user;
     return (
         <>
             <Navbar />
             <Routes>
-        
+
                 {/* RUTAS PUBLICAS */}
                 <Route path="/" element={<Home />} />
                 <Route path="/notice" element={<Home />} />
-                
+
                 <Route path="/notice/:id" element={<NoticeComplete />} />
                 <Route path="/notice/:category" element={<NoticePerCategory />} />
-
-                <Route path="/noticeCreate" element={<NoticeCreate />} />
-                <Route path="/noticeOptions" element={<NoticeOptions />} />
-
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/reset-password" element={<ResetPassword />} />
                 <Route path="/mockup" element={<MockupLogin />} />
 
-                {/* RUTAS PRIVADAS */}
-                <Route path="/admin" element={<Private />}>
-                    <Route path="/admin/perfil" element={<AdminPerfil />} />
-                </Route>
+
 
             </Routes>
+
+            {
+                (rol === "administrator") &&
+                <Routes>
+                    {/* RUTAS PRIVADAS */}
+
+                    <Route path="/noticeCreate" element={<NoticeCreate />} />
+                    <Route path="/noticeOptions" element={<NoticeOptions />} />
+
+
+                    <Route path="/admin" element={<Private />}>
+                        <Route path="/admin/perfil" element={<AdminPerfil />} />
+                    </Route>
+
+                </Routes>
+            }
+
+
+
             <Footer />
         </>
     )
