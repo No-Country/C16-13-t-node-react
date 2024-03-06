f4a6df64d908c480f611d9a2bd3bb60f9e1e4c12
JosueXA <josue.xicotencatl.a@gmail.com>
2024-03-06 11:36:09 -0600

    modify table plain on table responsive

--- FRONTEND/src/auth/Login.tsx
+++ FRONTEND/src/auth/Login.tsx
@@ -47,7 +47,7 @@ export const Login = () => {
 
         <div className="min-h-screen flex flex-col justify-center items-center pt-6 sm:pt-0 bg-[#FFF]">
 
-          <div className="absolute top-12 sm:top-32">
+          <div className="absolute top-12 sm:top-56">
             <h2 className="font-bold text-3xl text-[--secondary-500]">
               Hola, te damos la <span className="bg-[--primary-100] text-white px-2 rounded-lg">bienvenida!</span>
             </h2>
