12ee06148be923106aa72995d8cbb2a452c66b5a
JosueXA <josue.xicotencatl.a@gmail.com>
2024-03-06 08:38:40 -0600

    Styles modify footer component and social media

--- FRONTEND/src/components/Footer.tsx
+++ FRONTEND/src/components/Footer.tsx
@@ -4,8 +4,8 @@ import { SocialMedia } from "./utils"
 export const Footer = () => {
   return (
     <div className="bg-[--secondary-300] flex flex-row justify-around text-xs text-[--secundary] px-2 py-10 font-semibold">
-        <li className="list-none"><Link to="/">Sobre nosotros</Link></li>
-        <li className="list-none"><Link to="/">Información legal</Link></li>
+        <li className="list-none text-[--primary] hover:text-[--warning-100] hover:cursor-pointer"><Link to="/">Sobre nosotros</Link></li>
+        <li className="list-none text-[--primary] hover:text-[--warning-100] hover:cursor-pointer"><Link to="/">Información legal</Link></li>
         <SocialMedia/>
     </div>
   )
